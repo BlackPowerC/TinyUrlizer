36a971c6d3078fc884782e501a7e583493ef8d28
jojo <jordy.fatigba@theopentrade.com>
2022-06-26 14:55:59 +0000

    Ajout d'un record comme formulaire d'envoi d'une url à raccourcir.

---
+++ src/main/java/com/blackpowerc/tinyurlizer/ShrinkedURLForm.java
@@ -0,0 +1,9 @@
+package com.blackpowerc.tinyurlizer;
+
+/**
+ * Formulaire d'envoi d'une URL à raccourcir.
+ *
+ * @author jordy jordy.fatigba@theopentrade.com
+ */
+public record ShrinkedURLForm(String sourceUrl) {
+}
