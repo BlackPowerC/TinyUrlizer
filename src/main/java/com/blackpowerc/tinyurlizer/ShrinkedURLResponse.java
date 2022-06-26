05672f917d5b2a2b8bdc2e9c4a4a65ec49153c76
jojo <jordy.fatigba@theopentrade.com>
2022-06-26 14:57:57 +0000

    Ajout d'un record comme objet de réponse à retourner par l'API Rest.

---
+++ src/main/java/com/blackpowerc/tinyurlizer/ShrinkedURLResponse.java
@@ -0,0 +1,9 @@
+package com.blackpowerc.tinyurlizer;
+
+/**
+ * Objet réponse à utiliser pour envoyer l'URL raccourcie.
+ *
+ * @author jordy jordy.fatigba@theopentrade.com
+ */
+public record ShrinkedURLResponse(String shrinkedUrl) {
+}
