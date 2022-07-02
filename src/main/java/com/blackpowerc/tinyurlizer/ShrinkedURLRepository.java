b5d97dc983f546e918d6564544edda99b1741c7e
jojo <jordy.fatigba@theopentrade.com>
2022-07-02 10:58:34 +0000

    Mise à jour du repository de ShrinkedURL avec le type d'id en String.

--- src/main/java/com/blackpowerc/tinyurlizer/ShrinkedURLRepository.java
+++ src/main/java/com/blackpowerc/tinyurlizer/ShrinkedURLRepository.java
@@ -1,17 +1,15 @@
 package com.blackpowerc.tinyurlizer;
 
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-import java.util.UUID;
 
 /**
  *
  * @author jordy jordy.fatigba@theopentrade.com
  */
 @Repository
-public interface ShrinkedURLRepository extends JpaRepository<ShrinkedURL, UUID> {
+public interface ShrinkedURLRepository extends JpaRepository<ShrinkedURL, String> {
     Optional<ShrinkedURL> findBySourceUrl(String sourceUrl) ;
 }
