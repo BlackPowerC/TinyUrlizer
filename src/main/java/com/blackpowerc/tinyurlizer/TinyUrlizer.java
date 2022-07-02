6930859e67da260c36a796010836f1c04d676ee9
jojo <jordy.fatigba@theopentrade.com>
2022-07-02 15:05:52 +0000

    Ajout de la classe principale pour lancer l'application.

---
+++ src/main/java/com/blackpowerc/tinyurlizer/TinyUrlizer.java
@@ -0,0 +1,12 @@
+package com.blackpowerc.tinyurlizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyUrlizer
+{
+    public static void main(String[] args) {
+        SpringApplication.run(TinyUrlizer.class, args) ;
+    }
+}
